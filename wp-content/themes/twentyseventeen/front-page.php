23b923dc796a4aee105d4fa591eab625e305604a
subu <emailssubu87@gmail.com>
2018-01-20 17:29:37 +0530

    new content added

--- wp-content/themes/twentyseventeen/front-page.php
+++ wp-content/themes/twentyseventeen/front-page.php
@@ -16,6 +16,10 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+	
+<h1>PHP VERSION : <?php echo phpversion(); ?> </h1>
+
+<marquee> this is the demo version worked by subu </marquee>
 
 		<?php
 		// Show the selected front page content.
