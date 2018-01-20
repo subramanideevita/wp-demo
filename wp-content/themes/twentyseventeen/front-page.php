6dafc934559a90a92168364fb939d569a0242287
subramanideevita <subramanis@deevita.com>
2018-01-20 17:49:53 +0530

    another chage

--- wp-content/themes/twentyseventeen/front-page.php
+++ wp-content/themes/twentyseventeen/front-page.php
@@ -17,7 +17,7 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 	
-<h1>PHP VERSION : <?php echo phpversion(); ?> </h1>
+<h1>PHP VERSION : new one </h1>
 
 <marquee> this is the demo version worked by subu </marquee>
 
